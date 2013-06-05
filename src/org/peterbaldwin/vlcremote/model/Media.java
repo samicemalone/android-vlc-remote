616b4fc3c67841fc4f6cbc62692ee14d4ca6bc16
Sam Malone <samicemalone@gmail.com>
2013-06-05 09:37:52 +0100

    Fixed the Status being passed to receivers being null due to not implementing Serializable on all the classes Status depends on. Track is Serializable but Media was not.

--- src/org/peterbaldwin/vlcremote/model/Media.java
+++ src/org/peterbaldwin/vlcremote/model/Media.java
@@ -1,14 +1,30 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/*-
+ *  Copyright (C) 2013 Sam Malone   
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.peterbaldwin.vlcremote.model;
+
+import java.io.Serializable;
 
 /**
  *
  * @author Sam Malone
  */
-public abstract class Media implements PlaylistItem {
+public abstract class Media implements Serializable, PlaylistItem {
+    
+    private static final long serialVersionUID = 1L;
     
     protected int mId;
     protected boolean mCurrent;
