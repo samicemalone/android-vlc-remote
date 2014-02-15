d2982f56e8f378d6f1bbeaf23706d29166b5af2e
Sam Malone <samicemalone@gmail.com>
2014-02-15 05:15:26 +0000

    Removed broadcast receiver from StatusService. handleMessage now calls a separate method for each message instead of one large switch case (e.g. handleStatus, handleArt,...). Notifications changes are now instantiated from handleStatus. Status now has a equalsState method to compare a status state.

--- src/org/peterbaldwin/vlcremote/service/StatusService.java
+++ src/org/peterbaldwin/vlcremote/service/StatusService.java
@@ -18,20 +18,15 @@
 package org.peterbaldwin.vlcremote.service;
 
 import android.app.Service;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.IBinder;
-import android.os.Looper;
 import android.os.Message;
 import android.os.Process;
-import android.text.TextUtils;
 import android.util.Log;
 import java.io.IOException;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -96,13 +91,11 @@ public class StatusService extends Service implements Handler.Callback {
 
     private AtomicInteger mSequenceNumber;
     
-    private StatusReceiver mStatusReceiver;
+    private String mLastState;
     
     private String mLastFileName;
     
     private boolean mUpdateNotification;
-    
-    private boolean mWasPaused;
     
     @Override
     public void onCreate() {
@@ -121,16 +114,10 @@ public class StatusService extends Service implements Handler.Callback {
         // Create a separate thread for commands to improve latency
         // (commands shouldn't have to wait for partially complete reads).
         mCommandHandler = startHandlerThread("CommandThread");
-        
-        mStatusReceiver = new StatusReceiver();
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(Intents.ACTION_STATUS);
-        registerReceiver(mStatusReceiver, filter);
     }
 
     @Override
     public void onDestroy() {
-        unregisterReceiver(mStatusReceiver);
         stopHandlerThread(mNotificationHandler);
         stopHandlerThread(mStatusHandler);
         stopHandlerThread(mCommandHandler);
@@ -141,14 +128,11 @@ public class StatusService extends Service implements Handler.Callback {
     private Handler startHandlerThread(String name) {
         HandlerThread thread = new HandlerThread(name, Process.THREAD_PRIORITY_BACKGROUND);
         thread.start();
-        Looper looper = thread.getLooper();
-        Handler.Callback callback = this;
-        return new Handler(looper, callback);
+        return new Handler(thread.getLooper(), this);
     }
 
     private void stopHandlerThread(Handler handler) {
-        Looper looper = handler.getLooper();
-        looper.quit();
+        handler.getLooper().quit();
     }
 
     @Override
@@ -157,17 +141,15 @@ public class StatusService extends Service implements Handler.Callback {
         Uri uri = (intent != null) ? intent.getData() : null;
         if (Intents.ACTION_STATUS.equals(action) && uri != null) {
             if (isCommand(uri)) {
-                // A command will change the status,
-                // so cancel any unsent requests to
-                // query the status
+                // A command will change the status, so cancel any unsent
+                // requests to query the status
                 mStatusHandler.removeMessages(HANDLE_STATUS);
             }
 
             if (isSeek(uri) || isVolume(uri)) {
                 if (isAbsoluteValue(uri)) {
-                    // Seeking to an absolute position or volume
-                    // invalidates any existing requests to change
-                    // the position or volume.
+                    // Seeking to an absolute position or volume invalidates
+                    // any existing requests to change the position or volume
                     mCommandHandler.removeMessages(HANDLE_STATUS);
                 }
             }
@@ -181,9 +163,8 @@ public class StatusService extends Service implements Handler.Callback {
                 handler.sendMessage(msg);
             }
         } else if (Intents.ACTION_ART.equals(action) && uri != null) {
-            int sequenceNumber = mSequenceNumber.get();
-            Message msg = mAlbumArtHandler.obtainMessage(HANDLE_ALBUM_ART, sequenceNumber, -1, uri);
-            msg.sendToTarget();
+            int seqNumber = mSequenceNumber.get();
+            mAlbumArtHandler.obtainMessage(HANDLE_ALBUM_ART, seqNumber, -1, uri).sendToTarget();
         } else if (Intents.ACTION_NOTIFICATION_CANCEL.equals(action)) {
             NotificationControls.cancel(this);
         } else if (Intents.ACTION_NOTIFICATION_CREATE.equals(action)) {
@@ -199,133 +180,140 @@ public class StatusService extends Service implements Handler.Callback {
     @Override
     public boolean handleMessage(Message msg) {
         switch (msg.what) {
-            case HANDLE_STATUS: {
-                Uri uri = (Uri) msg.obj;
-                MediaServer server = new MediaServer(this, uri);
-                if(!server.getAuthority().equals(mAuthority)) {
-                    mAuthority = server.getAuthority();
-                    USE_XML_STATUS = false; // new server so try json first
-                }
-                int sequenceNumber = msg.arg1;
-                int flags = msg.arg2;
-                if (sequenceNumber == mSequenceNumber.get()) {
-                    boolean setResumeOnIdle = ((flags & Intents.FLAG_SET_RESUME_ON_IDLE) != 0);
-                    boolean onlyIfPlaying = ((flags & Intents.FLAG_ONLY_IF_PLAYING) != 0);
-                    boolean onlyIfPaused = ((flags & Intents.FLAG_ONLY_IF_PAUSED) != 0);
-                    boolean conditional = onlyIfPlaying || onlyIfPaused;
-                    try {
-                        if (conditional) {
-                            Status status = server.status().read();
-                            if (onlyIfPlaying && !status.isPlaying()) {
-                                return true;
-                            }
-                            if (onlyIfPaused && !status.isPaused()) {
-                                return true;
-                            }
-                        }
-                        Status status = server.status(uri).read();
-                        if (sequenceNumber == mSequenceNumber.get()) {
-                            sendBroadcast(Intents.status(status));
-                            if (isCommand(uri)) {
-                                // Check the status again after the command
-                                // has had time to take effect.
-                                msg = mStatusHandler.obtainMessage(HANDLE_STATUS, sequenceNumber,
-                                        0, readOnly(uri));
-                                mStatusHandler.sendMessageDelayed(msg, 500);
-                            }
-                        } else {
-                            Log.d(TAG, "Dropped stale status response: " + uri);
-                        }
-                        if (setResumeOnIdle) {
-                            Preferences.get(this).setResumeOnIdle();
-                        }
-                    } catch (Throwable tr) {
-                        if(JsonContentHandler.FILE_NOT_FOUND.equals(tr.getMessage())) {
-                            USE_XML_STATUS = true;
-                        }
-                        String message = String.valueOf(tr);
-                        Log.e(TAG, message, tr);
-                        Intent broadcast = Intents.error(tr);
-                        broadcast.putExtra(Intents.EXTRA_FLAGS, flags);
-                        sendBroadcast(broadcast);
-                    }
-                } else {
-                    Log.d(TAG, "Dropped stale status request: " + uri);
-                }
+            case HANDLE_STATUS:
+                handleStatus(msg);
                 return true;
-            }
-            case HANDLE_ALBUM_ART: {
-                Uri uri = (Uri) msg.obj;
-                MediaServer server = new MediaServer(this, uri);
-                int sequenceNumber = msg.arg1;
-                if (sequenceNumber == mSequenceNumber.get()) {
-                    try {
-                        Bitmap bitmap = server.image(uri).read();
-                        if (sequenceNumber == mSequenceNumber.get()) {
-                            sendBroadcast(Intents.art(bitmap));
-                        } else {
-                            Log.d(TAG, "Dropped stale album art response: " + uri);
-                        }
-                    } catch (Throwable tr) {
-                        String message = String.valueOf(tr);
-                        Log.e(TAG, message, tr);
-                        sendBroadcast(Intents.error(tr));
-                    }
-                } else {
-                    Log.d(TAG, "Dropped stale album art request: " + uri);
-                }
+            case HANDLE_ALBUM_ART:
+                handleArt(msg);
                 return true;
-            }
-            case HANDLE_NOTIFICATION_CREATE: {
+            case HANDLE_NOTIFICATION_CREATE:
                 new NotificationControls(this).showLoading();
                 mUpdateNotification = true;
                 return true;
-            }
-            case HANDLE_NOTIFICATION: {
-                int sequenceNumber = msg.arg1;
-                if (sequenceNumber == mSequenceNumber.get()) {
-                    MediaServer server = new MediaServer(this, Preferences.get(this).getAuthority());
-                    Bitmap bitmap;
-                    try {
-                        bitmap = server.art().read();
-                    } catch(IOException ex) {
-                        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.albumart_mp_unknown);
-                    }
-                    new NotificationControls(this).show((Status) msg.obj, bitmap);
-                }
+            case HANDLE_NOTIFICATION:
+                handleNotification(msg);
                 return true;
-            }
-            case HANDLE_STOP: {
+            case HANDLE_STOP:
                 int startId = msg.arg1;
                 stopSelf(startId);
                 return true;
-            }
             default:
                 return false;
         }
     }
 
+    private void handleStatus(Message msg) {
+        Uri uri = (Uri) msg.obj;
+        MediaServer server = new MediaServer(this, uri);
+        if(!server.getAuthority().equals(mAuthority)) {
+            mAuthority = server.getAuthority();
+            USE_XML_STATUS = false; // new server so try json first
+        }
+        int seqNumber = msg.arg1;
+        int flags = msg.arg2;
+        if (seqNumber == mSequenceNumber.get()) {
+            boolean setResumeOnIdle = ((flags & Intents.FLAG_SET_RESUME_ON_IDLE) != 0);
+            boolean onlyIfPlaying = ((flags & Intents.FLAG_ONLY_IF_PLAYING) != 0);
+            boolean onlyIfPaused = ((flags & Intents.FLAG_ONLY_IF_PAUSED) != 0);
+            try {
+                if (onlyIfPlaying || onlyIfPaused) {
+                    Status status = server.status().read();
+                    if (onlyIfPlaying && !status.isPlaying()) {
+                        return;
+                    }
+                    if (onlyIfPaused && !status.isPaused()) {
+                        return;
+                    }
+                }
+                Preferences pref = Preferences.get(this);
+                Status status = server.status(uri).read();
+                if (seqNumber == mSequenceNumber.get()) {
+                    sendBroadcast(Intents.status(status));
+                    if((checkStatusChanged(status) || mUpdateNotification) && pref.isNotificationSet()) {
+                        Message n = mNotificationHandler.obtainMessage(HANDLE_NOTIFICATION, seqNumber, -1, status);
+                        n.sendToTarget();
+                        mUpdateNotification = false;
+                    }
+                    if (isCommand(uri)) {
+                        // Check the status again after the command has had time to take effect.
+                        msg = mStatusHandler.obtainMessage(HANDLE_STATUS, seqNumber, 0, readOnly(uri));
+                        mStatusHandler.sendMessageDelayed(msg, 500);
+                    }
+                } else {
+                    Log.d(TAG, "Dropped stale status response: " + uri);
+                }
+                if (setResumeOnIdle) {
+                    pref.setResumeOnIdle();
+                }
+            } catch (Throwable tr) {
+                if(JsonContentHandler.FILE_NOT_FOUND.equals(tr.getMessage())) {
+                    USE_XML_STATUS = true;
+                }
+                String message = String.valueOf(tr);
+                Log.e(TAG, message, tr);
+                Intent broadcast = Intents.error(tr);
+                broadcast.putExtra(Intents.EXTRA_FLAGS, flags);
+                sendBroadcast(broadcast);
+            }
+        } else {
+            Log.d(TAG, "Dropped stale status request: " + uri);
+        }
+    }
+    
+    private void handleArt(Message msg) {
+        Uri uri = (Uri) msg.obj;
+        MediaServer server = new MediaServer(this, uri);
+        int sequenceNumber = msg.arg1;
+        if (sequenceNumber == mSequenceNumber.get()) {
+            try {
+                Bitmap bitmap = server.image(uri).read();
+                if (sequenceNumber == mSequenceNumber.get()) {
+                    sendBroadcast(Intents.art(bitmap));
+                } else {
+                    Log.d(TAG, "Dropped stale album art response: " + uri);
+                }
+            } catch (Throwable tr) {
+                String message = String.valueOf(tr);
+                Log.e(TAG, message, tr);
+                sendBroadcast(Intents.error(tr));
+            }
+        } else {
+            Log.d(TAG, "Dropped stale album art request: " + uri);
+        }
+    }
+    
+    private void handleNotification(Message msg) {
+        int seqNumber = msg.arg1;
+        if (seqNumber == mSequenceNumber.get()) {
+            MediaServer server = new MediaServer(this, Preferences.get(this).getAuthority());
+            Bitmap bitmap;
+            try {
+                bitmap = server.art().read();
+            } catch(IOException ex) {
+                bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.albumart_mp_unknown);
+            }
+            new NotificationControls(this).show((Status) msg.obj, bitmap);
+        }
+    }
+    
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
     
-    private final class StatusReceiver extends BroadcastReceiver {    
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if(mUpdateNotification || Preferences.get(context).isNotificationSet()) {
-                Status status = (Status) intent.getSerializableExtra(Intents.EXTRA_STATUS);
-                if(mUpdateNotification || mWasPaused != status.isPaused() || !TextUtils.equals(status.getTrack().getName(), mLastFileName)) {
-                    mWasPaused = status.isPaused();
-                    mLastFileName = status.getTrack().getName();
-                    int seq = mSequenceNumber.get();
-                    Message msg = mNotificationHandler.obtainMessage(HANDLE_NOTIFICATION, seq, -1, status);
-                    msg.sendToTarget();
-                }
-                mUpdateNotification = false;
-            }
+    /**
+     * Check if the playback status has changed. If the status has changed,
+     * the new state will be stored.
+     * @param status Status
+     * @return true if status was changed, false otherwise
+     */
+    private boolean checkStatusChanged(Status status) {
+        if(!status.equalsState(mLastFileName, mLastState)) {
+            mLastFileName = status.getTrack().getName();
+            mLastState = status.getState();
+            return true;
         }
-        
-    }
+        return false;
+     }
+
 }
